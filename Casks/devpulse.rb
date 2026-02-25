8bcb82786553f0b7eaad1da7aab0e5d796daec04
Yusuke Otawa <yusuke.otawa@pivot.inc>
2026-02-25 20:34:42 +0900

    Update devpulse to v0.2.1

--- Casks/devpulse.rb
+++ Casks/devpulse.rb
@@ -1,6 +1,6 @@
 cask "devpulse" do
-  version "0.1.0"
-  sha256 "87d1f3ffb12af1808e4d62c28fac39d7dd1bc74cfc306b102b4a4c7e2444e928"
+  version "0.2.1"
+  sha256 "c3020fbbf0b481c270bdbbe99ba628d79889aa01f97674666a04d2fc2df9592d"
 
   url "https://github.com/tawachan/devpulse/releases/download/v#{version}/DevPulse.zip"
   name "DevPulse"
